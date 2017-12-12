6b23151548150554aaefe31396058b018a16541a
ryotozawa <b1016031@fun.ac.jp>
2017-12-12 21:30:25 +0900

    Ryo

--- app.py
+++ app.py
@@ -56,9 +56,9 @@ def handle_message(event):
     # send a message
     payload = {
         "utt": event.message.text,
-        "context": g.context,
+        "context": "",
         "place": "北海道",
-        "mode": "srtr"
+        "mode": "dialog"
     }
     KEY = '2f42326a4d52784249447133356f656338317a3373464a4c4d6c73506a462f72574331687568694a637641'
     endpoint = 'https://api.apigw.smt.docomo.ne.jp/dialogue/v1/dialogue?APIKEY=REGISTER_KEY'
@@ -66,11 +66,11 @@ def handle_message(event):
     s = requests.session()
     r = s.post(url, data=json.dumps(payload))
     res_json = json.loads(r.text)
-    g.context = res_json['context']
-    sentence = str(res_json['utt'])+str(event.course.type)
+    # g.context = res_json['context']
+    # sentence = str(res_json['utt'])+str(event.course.type)
     line_bot_api.reply_message(
         event.reply_token,
-        TextSendMessage(text=sentence)
+        TextSendMessage(text=res_json['utt'])
     )
 
 
